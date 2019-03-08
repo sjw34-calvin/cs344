74b949c6ead15bf85f8419beb39b9fff5880495e
sjw34-calvin <sjw34@students.calvin.edu>
2019-03-08 15:36:36 -0500

    upload lab06

--- lab06/lab06_3.py
+++ lab06/lab06_3.py
@@ -1,1 +1,20 @@
 # I could not get Keras to work.
+
+from keras.datasets import boston_housing
+(train_images, train_labels), (test_images, test_labels) = boston_housing.load_data()
+
+def print_structures():
+    print(
+        f'training images \
+            \n\tcount: {len(train_images)} \
+            \n\tdimensions: {train_images.ndim} \
+            \n\tshape: {train_images.shape} \
+            \n\tdata type: {train_images.dtype}\n\n',
+        f'testing images \
+            \n\tcount: {len(test_labels)} \
+            \n\tdimensions: {train_labels.ndim} \
+            \n\tshape: {test_labels.shape} \
+            \n\tdata type: {test_labels.dtype} \
+            \n\tvalues: {test_labels}\n',
+    )
+print_structures()
